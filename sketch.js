43a8ba8e3877deaf8d94bbb9bb22274ad0939f17
Justin Egan <gardenofegan@gmail.com>
2024-09-19 23:12:03 -0400

    much closer

--- sketch.js
+++ sketch.js
@@ -3,6 +3,9 @@ let poseNet;
 let poses = [];
 let stars = [];
 
+let smoothedPoses = [];
+const smoothingFactor = 0.8;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   video = createCapture(VIDEO);
@@ -26,7 +29,22 @@ function setup() {
 
   // Use a callback function for model loading
   poseNet = ml5.poseNet(video, modelReady);
-  poseNet.on('pose', gotPoses);
+  poseNet.on('pose', (results) => {
+    if (results.length > 0) {
+      if (smoothedPoses.length === 0) {
+        smoothedPoses = results;
+      } else {
+        results.forEach((pose, i) => {
+          if (!smoothedPoses[i]) smoothedPoses[i] = pose;
+          for (let j = 0; j < pose.pose.keypoints.length; j++) {
+            const point = pose.pose.keypoints[j];
+            smoothedPoses[i].pose[point.part].x = lerp(smoothedPoses[i].pose[point.part].x, point.position.x, 1 - smoothingFactor);
+            smoothedPoses[i].pose[point.part].y = lerp(smoothedPoses[i].pose[point.part].y, point.position.y, 1 - smoothingFactor);
+          }
+        });
+      }
+    }
+  });
 }
 
 function windowResized() {
@@ -48,18 +66,14 @@ function modelReady() {
   console.log('PoseNet model loaded');
 }
 
-function gotPoses(results) {
-  poses = results;
-}
-
 function draw() {
   // Draw space background
   background(0);
   drawStars();
   
   // Draw aliens for each detected pose
-  for (let i = 0; i < poses.length; i++) {
-    drawAlien(poses[i].pose);
+  for (let i = 0; i < smoothedPoses.length; i++) {
+    drawAlien(smoothedPoses[i].pose);
   }
 }
 
@@ -75,47 +89,51 @@ function drawAlien(pose) {
   // Calculate head size based on eye distance
   let headSize = dist(pose.leftEye.x, pose.leftEye.y, pose.rightEye.x, pose.rightEye.y) * 3;
 
+  push();
+  translate((pose.leftShoulder.x + pose.rightShoulder.x) / 2, (pose.leftShoulder.y + pose.rightShoulder.y) / 2);
+  
   // Draw body parts
-  drawHead(pose, headSize);
   drawBody(pose, headSize);
-  drawLimb(pose.leftShoulder, pose.leftElbow, pose.leftWrist, headSize, color(0, 200, 0)); // Left arm
-  drawLimb(pose.rightShoulder, pose.rightElbow, pose.rightWrist, headSize, color(0, 200, 0)); // Right arm
-  drawLimb(pose.leftHip, pose.leftKnee, pose.leftAnkle, headSize, color(0, 220, 0)); // Left leg
-  drawLimb(pose.rightHip, pose.rightKnee, pose.rightAnkle, headSize, color(0, 220, 0)); // Right leg
-  drawHand(pose.leftWrist, headSize, color(0, 160, 0)); // Left hand
-  drawHand(pose.rightWrist, headSize, color(0, 160, 0)); // Right hand
-  drawFoot(pose.leftAnkle, headSize, color(0, 180, 0)); // Left foot
-  drawFoot(pose.rightAnkle, headSize, color(0, 180, 0)); // Right foot
+  drawLimb(pose, pose.leftShoulder, pose.leftElbow, pose.leftWrist, headSize, color(0, 200, 0)); // Left arm
+  drawLimb(pose, pose.rightShoulder, pose.rightElbow, pose.rightWrist, headSize, color(0, 200, 0)); // Right arm
+  drawLimb(pose, pose.leftHip, pose.leftKnee, pose.leftAnkle, headSize, color(0, 220, 0)); // Left leg
+  drawLimb(pose, pose.rightHip, pose.rightKnee, pose.rightAnkle, headSize, color(0, 220, 0)); // Right leg
+  drawHand(pose, pose.leftWrist, headSize, color(0, 160, 0)); // Left hand
+  drawHand(pose, pose.rightWrist, headSize, color(0, 160, 0)); // Right hand
+  drawFoot(pose, pose.leftAnkle, headSize, color(0, 180, 0)); // Left foot
+  drawFoot(pose, pose.rightAnkle, headSize, color(0, 180, 0)); // Right foot
+  drawHead(pose, headSize);
+  
+  pop();
 }
 
 function drawHead(pose, size) {
   let nose = pose.nose;
-  if (nose.confidence > 0.5) {
-    push();
-    translate(nose.x, nose.y);
-    
-    // Alien head
-    fill(0, 255, 0);
-    ellipse(0, 0, size, size * 1.2);
-    
-    // Eyes
-    fill(0);
-    let eyeSize = size * 0.2;
-    ellipse(-size/4, -size/6, eyeSize, eyeSize * 1.5);
-    ellipse(size/4, -size/6, eyeSize, eyeSize * 1.5);
-    
-    // Mouth
-    fill(255);
-    arc(0, size/5, size/2, size/5, 0, PI);
-    
-    // Antennae
-    stroke(0, 255, 0);
-    strokeWeight(2);
-    line(0, -size/2, -size/4, -size*0.8);
-    line(0, -size/2, size/4, -size*0.8);
-    
-    pop();
-  }
+  push();
+  translate(nose.x - (pose.leftShoulder.x + pose.rightShoulder.x) / 2, 
+            nose.y - (pose.leftShoulder.y + pose.rightShoulder.y) / 2);
+  
+  // Alien head
+  fill(0, 255, 0);
+  ellipse(0, 0, size, size * 1.2);
+  
+  // Eyes
+  fill(0);
+  let eyeSize = size * 0.2;
+  ellipse(-size/4, -size/6, eyeSize, eyeSize * 1.5);
+  ellipse(size/4, -size/6, eyeSize, eyeSize * 1.5);
+  
+  // Mouth
+  fill(255);
+  arc(0, size/5, size/2, size/5, 0, PI);
+  
+  // Antennae
+  stroke(0, 255, 0);
+  strokeWeight(2);
+  line(0, -size/2, -size/4, -size*0.8);
+  line(0, -size/2, size/4, -size*0.8);
+  
+  pop();
 }
 
 function drawBody(pose, headSize) {
@@ -124,64 +142,56 @@ function drawBody(pose, headSize) {
   let leftHip = pose.leftHip;
   let rightHip = pose.rightHip;
   
-  if (leftShoulder.confidence > 0.5 && rightShoulder.confidence > 0.5 &&
-      leftHip.confidence > 0.5 && rightHip.confidence > 0.5) {
-    let bodyWidth = dist(leftShoulder.x, leftShoulder.y, rightShoulder.x, rightShoulder.y);
-    let bodyHeight = dist((leftShoulder.x + rightShoulder.x) / 2, (leftShoulder.y + rightShoulder.y) / 2,
-                          (leftHip.x + rightHip.x) / 2, (leftHip.y + rightHip.y) / 2);
-    
-    push();
-    translate((leftShoulder.x + rightShoulder.x) / 2, (leftShoulder.y + rightShoulder.y) / 2);
-    fill(0, 240, 0);
-    ellipse(0, bodyHeight / 2, bodyWidth * 1.2, bodyHeight * 1.1);
-    pop();
-  }
+  let bodyWidth = dist(leftShoulder.x, leftShoulder.y, rightShoulder.x, rightShoulder.y);
+  let bodyHeight = dist((leftShoulder.x + rightShoulder.x) / 2, (leftShoulder.y + rightShoulder.y) / 2,
+                        (leftHip.x + rightHip.x) / 2, (leftHip.y + rightHip.y) / 2);
+  
+  fill(0, 240, 0);
+  ellipse(0, bodyHeight / 2, bodyWidth * 1.2, bodyHeight * 1.1);
 }
 
-function drawLimb(joint1, joint2, joint3, headSize, color) {
-  if (joint1.confidence > 0.5 && joint2.confidence > 0.5 && joint3.confidence > 0.5) {
-    let upperLength = dist(joint1.x, joint1.y, joint2.x, joint2.y);
-    let lowerLength = dist(joint2.x, joint2.y, joint3.x, joint3.y);
-    let limbWidth = headSize * 0.2;
-    
-    // Upper part
-    push();
-    translate(joint1.x, joint1.y);
-    let angle1 = atan2(joint2.y - joint1.y, joint2.x - joint1.x);
-    rotate(angle1);
-    fill(color);
-    ellipse(upperLength / 2, 0, upperLength, limbWidth);
-    pop();
-    
-    // Lower part
-    push();
-    translate(joint2.x, joint2.y);
-    let angle2 = atan2(joint3.y - joint2.y, joint3.x - joint2.x);
-    rotate(angle2);
-    fill(color);
-    ellipse(lowerLength / 2, 0, lowerLength, limbWidth * 0.8);
-    pop();
-  }
+function drawLimb(pose, joint1, joint2, joint3, headSize, color) {
+  let upperLength = dist(joint1.x, joint1.y, joint2.x, joint2.y);
+  let lowerLength = dist(joint2.x, joint2.y, joint3.x, joint3.y);
+  let limbWidth = headSize * 0.2;
+  
+  // Upper part
+  push();
+  translate(joint1.x - (pose.leftShoulder.x + pose.rightShoulder.x) / 2, 
+            joint1.y - (pose.leftShoulder.y + pose.rightShoulder.y) / 2);
+  let angle1 = atan2(joint2.y - joint1.y, joint2.x - joint1.x);
+  rotate(angle1);
+  fill(color);
+  ellipse(upperLength / 2, 0, upperLength, limbWidth);
+  pop();
+  
+  // Lower part
+  push();
+  translate(joint2.x - (pose.leftShoulder.x + pose.rightShoulder.x) / 2, 
+            joint2.y - (pose.leftShoulder.y + pose.rightShoulder.y) / 2);
+  let angle2 = atan2(joint3.y - joint2.y, joint3.x - joint2.x);
+  rotate(angle2);
+  fill(color);
+  ellipse(lowerLength / 2, 0, lowerLength, limbWidth * 0.8);
+  pop();
 }
 
-function drawHand(wrist, headSize, color) {
-  if (wrist.confidence > 0.5) {
-    push();
-    translate(wrist.x, wrist.y);
-    fill(color);
-    ellipse(0, 0, headSize * 0.3, headSize * 0.3);
-    pop();
-  }
+function drawHand(pose, wrist, headSize, color) {
+  push();
+  translate(wrist.x - (pose.leftShoulder.x + pose.rightShoulder.x) / 2, 
+            wrist.y - (pose.leftShoulder.y + pose.rightShoulder.y) / 2);
+  fill(color);
+  ellipse(0, 0, headSize * 0.3, headSize * 0.3);
+  pop();
 }
 
-function drawFoot(ankle, headSize, color) {
-  if (ankle.confidence > 0.5) {
-    push();
-    translate(ankle.x, ankle.y);
-    fill(color);
-    ellipse(0, headSize * 0.1, headSize * 0.4, headSize * 0.2);
-    pop();
-  }
+function drawFoot(pose, ankle, headSize, color) {
+  push();
+  translate(ankle.x - (pose.leftShoulder.x + pose.rightShoulder.x) / 2, 
+            ankle.y - (pose.leftShoulder.y + pose.rightShoulder.y) / 2);
+  fill(color);
+  ellipse(0, headSize * 0.1, headSize * 0.4, headSize * 0.2);
+  pop();
 }
 
 function drawConnection(part1, part2) {
